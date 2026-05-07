3ff58d49a09b8bad792ec9138de9f512f89f4af8
aristaw-hub <aristaw@gmail.com>
2026-05-07 22:16:09 +0800

    test

--- main.tf
+++ main.tf
@@ -83,6 +83,10 @@ resource "aws_s3_bucket_lifecycle_configuration" "s3_tf" {
 
     filter {}
 
+    abort_incomplete_multipart_upload {
+      days_after_initiation = 7
+    }
+
     expiration {
       days = 90
     }
